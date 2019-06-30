a25a8ac7b24878677e73af1e4c8f352e3027cda1
Sargis <s.aghabekyan@yandex.ru>
2019-06-30 18:32:35 +0400

    added deployment scripts

---
+++ client/deploy.sh
@@ -0,0 +1,17 @@
+  docker build -t aghabekyan374/multi-client:latest -t aghabekyan374/multi-client:$SHA ./client/Dockerfile ./client
+  docker build -t aghabekyan374/multi-server:latest -t aghabekyan374/multi-server:$SHA ./server/Dockerfile ./server
+  docker build -t aghabekyan374/multi-worker:latest -t aghabekyan374/multi-worker:$SHA ./worker/Dockerfile ./worker
+  
+  # Take those Images and push them to docker hub
+  docker push aghabekyan374/multi-client:latest
+  docker push aghabekyan374/multi-server:latest
+  docker push aghabekyan374/multi-worker:latest
+
+  docker push aghabekyan374/multi-client:$SHA
+  docker push aghabekyan374/multi-server:$SHA
+  docker push aghabekyan374/multi-worker:$SHA
+
+  kubectl apply -f k8s
+  kubectl set image deployments/server-deployment server=aghabekyan374/multi-server:$SHA
+  kubectl set image deployments/client-deployment client=aghabekyan374/multi-client:$SHA
+  kubectl set image deployments/worker-deployment worker=aghabekyan374/multi-worker:$SHA
