a8abdfce7b855b46a2886e036a307d4200fd0bf4
Sargis <s.aghabekyan@yandex.ru>
2019-06-30 19:14:19 +0400

    develop test branch

--- deploy.sh
+++ deploy.sh
@@ -2,7 +2,6 @@ docker build -t aghabekyan374/multi-client:latest -t aghabekyan374/multi-client:
 docker build -t aghabekyan374/multi-server:latest -t aghabekyan374/multi-server:$SHA -f ./server/Dockerfile ./server
 docker build -t aghabekyan374/multi-worker:latest -t aghabekyan374/multi-worker:$SHA -f ./worker/Dockerfile ./worker
 
-# Take those Images and push them to docker hub
 docker push aghabekyan374/multi-client:latest
 docker push aghabekyan374/multi-server:latest
 docker push aghabekyan374/multi-worker:latest
