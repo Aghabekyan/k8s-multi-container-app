45a97cf3d8a98c3dd0eb40066ab0dade9dc21aeb
Sargis <s.aghabekyan@yandex.ru>
2019-06-30 18:58:27 +0400

    changed deploy file

--- deploy.sh
+++ deploy.sh
@@ -1,17 +1,17 @@
-  docker build -t aghabekyan374/multi-client:latest -t aghabekyan374/multi-client:$SHA ./client/Dockerfile ./client
-  docker build -t aghabekyan374/multi-server:latest -t aghabekyan374/multi-server:$SHA ./server/Dockerfile ./server
-  docker build -t aghabekyan374/multi-worker:latest -t aghabekyan374/multi-worker:$SHA ./worker/Dockerfile ./worker
-  
-  # Take those Images and push them to docker hub
-  docker push aghabekyan374/multi-client:latest
-  docker push aghabekyan374/multi-server:latest
-  docker push aghabekyan374/multi-worker:latest
+docker build -t aghabekyan374/multi-client:latest -t aghabekyan374/multi-client:$SHA -f ./client/Dockerfile ./client
+docker build -t aghabekyan374/multi-server:latest -t aghabekyan374/multi-server:$SHA -f ./server/Dockerfile ./server
+docker build -t aghabekyan374/multi-worker:latest -t aghabekyan374/multi-worker:$SHA -f ./worker/Dockerfile ./worker
 
-  docker push aghabekyan374/multi-client:$SHA
-  docker push aghabekyan374/multi-server:$SHA
-  docker push aghabekyan374/multi-worker:$SHA
+# Take those Images and push them to docker hub
+docker push aghabekyan374/multi-client:latest
+docker push aghabekyan374/multi-server:latest
+docker push aghabekyan374/multi-worker:latest
 
-  kubectl apply -f k8s
-  kubectl set image deployments/server-deployment server=aghabekyan374/multi-server:$SHA
-  kubectl set image deployments/client-deployment client=aghabekyan374/multi-client:$SHA
-  kubectl set image deployments/worker-deployment worker=aghabekyan374/multi-worker:$SHA
+docker push aghabekyan374/multi-client:$SHA
+docker push aghabekyan374/multi-server:$SHA
+docker push aghabekyan374/multi-worker:$SHA
+
+kubectl apply -f k8s
+kubectl set image deployments/server-deployment server=aghabekyan374/multi-server:$SHA
+kubectl set image deployments/client-deployment client=aghabekyan374/multi-client:$SHA
+kubectl set image deployments/worker-deployment worker=aghabekyan374/multi-worker:$SHA
